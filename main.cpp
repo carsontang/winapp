2f8e3be42d12e7f5e017205988d05ca0c8aa268c
Carson <carson@visor.gg>
2018-08-27 18:59:45 -0700

    add windows

--- main.cpp
+++ main.cpp
@@ -1,17 +1,108 @@
-#include <windows.h>    // include the basic windows header file
+// #include <windows.h>    // include the basic windows header file
+
+// // the entry point for any Windows program
+// int WINAPI WinMain(HINSTANCE hInstance,
+//                    HINSTANCE hPrevInstance,
+//                    LPSTR lpCmdLine,
+//                    int nShowCmd)
+// {
+//     // create a "Hello World" message box using MessageBox()
+//     MessageBoxW(NULL,
+//                L"Hello World!",
+//                L"Just another Hello World program!",
+//                MB_ICONERROR | MB_OK);
+
+//     // return 0 to Windows
+//     return 0;
+// }
+
+// include the basic windows header file
+#include <windows.h>
+#include <windowsx.h>
+
+// the WindowProc function prototype
+LRESULT CALLBACK WindowProc(HWND hWnd,
+                         UINT message,
+                         WPARAM wParam,
+                         LPARAM lParam);
 
 // the entry point for any Windows program
 int WINAPI WinMain(HINSTANCE hInstance,
                    HINSTANCE hPrevInstance,
                    LPSTR lpCmdLine,
-                   int nShowCmd)
+                   int nCmdShow)
 {
-    // create a "Hello World" message box using MessageBox()
-    MessageBoxW(NULL,
-               L"Hello World!",
-               L"Just another Hello World program!",
-               MB_ICONEXCLAMATION | MB_OK);
+    // the handle for the window, filled by a function
+    HWND hWnd;
+    // this struct holds information for the window class
+    WNDCLASSEX wc;
 
-    // return 0 to Windows
-    return 0;
+    // clear out the window class for use
+    ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+    // fill in the struct with the needed information
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    wc.lpszClassName = "WindowClass1";
+
+    // register the window class
+    RegisterClassEx(&wc);
+
+    // create the window and use the result as the handle
+    hWnd = CreateWindowEx(WS_EX_ACCEPTFILES,
+                          "WindowClass1",    // name of the window class
+                          "Our First Windowed Program",   // title of the window
+                          WS_OVERLAPPEDWINDOW,    // window style
+                          300,    // x-position of the window
+                          300,    // y-position of the window
+                          500,    // width of the window
+                          400,    // height of the window
+                          NULL,    // we have no parent window, NULL
+                          NULL,    // we aren't using menus, NULL
+                          hInstance,    // application handle
+                          NULL);    // used with multiple windows, NULL
+
+    // display the window on the screen
+    ShowWindow(hWnd, nCmdShow);
+
+    // enter the main loop:
+
+    // this struct holds Windows event messages
+    MSG msg;
+
+    // wait for the next message in the queue, store the result in 'msg'
+    while(GetMessage(&msg, NULL, 0, 0))
+    {
+        // translate keystroke messages into the right format
+        TranslateMessage(&msg);
+
+        // send the message to the WindowProc function
+        DispatchMessage(&msg);
+    }
+
+    // return this part of the WM_QUIT message to Windows
+    return msg.wParam;
+}
+
+// this is the main message handler for the program
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    // sort through and find what code to run for the message given
+    switch(message)
+    {
+        // this message is read when the window is closed
+        case WM_DESTROY:
+            {
+                // close the application entirely
+                PostQuitMessage(0);
+                return 0;
+            } break;
+    }
+
+    // Handle any messages the switch statement didn't
+    return DefWindowProc (hWnd, message, wParam, lParam);
 }
