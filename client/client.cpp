028ecacf41a4d6ff67052d3ba7ddf9946e77ad77
Carson <carson@visor.gg>
2018-09-13 18:15:09 -0700

    write buffer to named pipe

--- client/client.cpp
+++ client/client.cpp
@@ -1,4 +1,6 @@
 #include <windows.h>
+#include <stdint.h>
+#include <stdio.h>
 
 int main(void)
 {
@@ -15,11 +17,48 @@ int main(void)
                        NULL);
     if (hPipe != INVALID_HANDLE_VALUE)
     {
-        WriteFile(hPipe,
-                  "Hello Pipe\n",
-                  12,   // = length of string + terminating '\0' !!!
+        HANDLE hImage = CreateFileW(
+            L"C:\\Users\\Carson Tang\\Documents\\tab.bmp",
+            GENERIC_READ | GENERIC_WRITE,
+            0, // the pipe can only be opened once
+            nullptr,
+            OPEN_EXISTING,
+            0,
+            nullptr);
+        
+        if (hImage == INVALID_HANDLE_VALUE) {
+            OutputDebugStringA("[VisorGG] Unable to create file");
+            return false;
+        }
+
+        HANDLE hFilemap = CreateFileMapping(
+            hImage,
+            nullptr,
+            PAGE_READWRITE,
+            0,
+            1920 * 1040 * 4,
+            nullptr);
+        
+        if (hFilemap == INVALID_HANDLE_VALUE) {
+            OutputDebugStringA("[VisorGG] Unable to create file map");
+            return false;
+        }
+
+        uint8_t *image_buffer = (uint8_t*)MapViewOfFile(hFilemap, FILE_MAP_WRITE, 0, 0, 0);
+
+        DWORD total_written = 0;
+
+        while (WriteFile(hPipe,
+                  image_buffer,
+                  1920 * 1040 * 4,   // = length of string + terminating '\0' !!!
                   &dwWritten,
-                  NULL);
+                  NULL) != FALSE) {
+            total_written += dwWritten;
+            if (total_written == (1920 * 1040 * 4)) {
+                printf("Done writing buffer to server\n");
+                break;
+            }
+        }
 
         CloseHandle(hPipe);
     }
--- server/server.cpp
+++ server/server.cpp
@@ -1,33 +1,85 @@
 #include <windows.h>
 #include <stdio.h>
+#include <stdint.h>
+
+#define BUFFER_SIZE 1920
+
+bool persist_buffer(void *buffer, int filecount)
+{
+    HANDLE hFile = CreateFile(TEXT("C:\\Users\\Carson Tang\\Documents\\buffer.bmp"),
+        GENERIC_WRITE,
+        0, // the pipe can only be opened once
+        nullptr,
+        CREATE_ALWAYS,
+        0,
+        nullptr);
+    
+    DWORD dwWritten;
+    DWORD total_bytes = 0;
+    while (WriteFile(hFile, buffer, 1920*1040*4, &dwWritten, NULL) != FALSE) {
+        total_bytes += dwWritten;
+    }
+
+    CloseHandle(hFile);
+    if (total_bytes == (1920 * 1040 * 4)) {
+        printf("Successfully persisted a bitmap!\n");
+        return true;
+    }
+    return false;
+}
 
 int main(void)
 {
     HANDLE hPipe;
-    char buffer[1024];
+    uint8_t buffer[BUFFER_SIZE];
     DWORD dwRead;
 
+    OutputDebugString(TEXT("[VisorGG] Before pipe creation"));
 
     hPipe = CreateNamedPipe(TEXT("\\\\.\\pipe\\Pipe"),
                             PIPE_ACCESS_DUPLEX,
                             PIPE_TYPE_BYTE | PIPE_READMODE_BYTE | PIPE_WAIT,   // FILE_FLAG_FIRST_PIPE_INSTANCE is not needed but forces CreateNamedPipe(..) to fail if the pipe already exists...
                             1,
-                            1024 * 16,
-                            1024 * 16,
+                            BUFFER_SIZE,
+                            BUFFER_SIZE,
                             NMPWAIT_USE_DEFAULT_WAIT,
                             NULL);
+    int filecount = 0;
+    OutputDebugString(TEXT("[VisorGG] Before loop"));
     while (hPipe != INVALID_HANDLE_VALUE)
     {
+        OutputDebugString(TEXT("[VisorGG] Inside loop"));
         if (ConnectNamedPipe(hPipe, NULL) != FALSE)   // wait for someone to connect to the pipe
         {
-            while (ReadFile(hPipe, buffer, sizeof(buffer) - 1, &dwRead, NULL) != FALSE)
-            {
-                /* add terminating zero */
-                buffer[dwRead] = '\0';
+            DWORD num_read_bytes = 0;
+            HANDLE hFile = CreateFile(TEXT("C:\\Users\\Carson Tang\\Documents\\buffer.bmp"),
+                    GENERIC_WRITE,
+                    0, // the pipe can only be opened once
+                    nullptr,
+                    CREATE_ALWAYS,
+                    0,
+                    nullptr);
+            
+            DWORD dwPos;
+            DWORD dwWritten;
 
-                /* do something with data in buffer */
-                printf("%s", buffer);
+            while (ReadFile(hPipe, buffer, sizeof(buffer), &dwRead, NULL) && dwRead > 0)
+            {
+                dwPos = SetFilePointer(hFile, 0, NULL, FILE_END);
+                LockFile(hFile, dwPos, 0, dwRead, 0);
+                WriteFile(hFile, buffer, dwRead, &dwWritten, NULL);
+                UnlockFile(hFile, dwPos, 0, dwRead, 0);
+                // num_read_bytes += dwRead;
+                // if (num_read_bytes == (1920 * 1040 * 4)) {
+                //     bool persisted = persist_buffer(buffer, filecount);
+                //     if (!persisted) {
+                //         printf("Unable to persist buffer\n");
+                //     } else {
+                //         filecount++;
+                //     }
+                // }
             }
+            CloseHandle(hFile);
         }
 
         DisconnectNamedPipe(hPipe);
